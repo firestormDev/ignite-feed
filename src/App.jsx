60623acdbd8ec0796ed7d87d3ff35eebd8dc5051
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2024-03-28 19:13:56 -0300

    feat: Implementing Post Properties

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,65 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css'
 import './global.css'
 
+// author : { avatar_url: " ", name: "", role:"",}
+// publishedAt: Date
+// content: String
+
+const posts= [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https:github.com/firestormdev.png',
+      name: 'Luiz Tinti',
+      role: 'Web Developer'
+    },
+
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋',},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',},
+      {type: 'link', content: 'jane.design / doctorcare'},
+    ],
+
+    publishedAt: new Date('2024-03-26 20:00:01'),
+
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https:github.com/thiagol1r4.png',
+      name: 'Thiago Nobre',
+      role: 'Senior Developer'
+    },
+
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋',},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀',},
+      {type: 'link', content: 'jane.design / doctorcare'},
+    ],
+
+    publishedAt: new Date('2023-02-03 20:00:01'),
+
+  },
+];
+
+
 export function App() {
   return (
     <div>
-      <Header/>
-    
-     <div className={styles.wrapper}>
-      <Sidebar/>
-
-      <main>
-        <Post author="luiz" 
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit voluptas aspernatur quidem ipsum. Culpa, earum praesentium omnis aut laboriosam modi delectus, sed consectetur sint ipsum quidem eveniet, animi itaque similique."
-        />
-        <Post author="Gabriel" 
-        content="teste."
-        />
-      </main>
-     </div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />)
+          })}
+        </main>
+      </div>
     </div>
   )
 }
