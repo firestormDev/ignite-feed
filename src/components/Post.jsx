bb43066d18b7b5f4c032f26bb8e2941b49dfabf6
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2024-04-09 20:24:23 -0300

    feat: Implementing comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,10 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post dahora'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -24,9 +25,13 @@ export function Post({ author, publishedAt, content }) {
 
     function handleCreateNewComment() {
         event.preventDefault()
+        
+        setComments([...comments, newCommentText]);
+        setNewCommentText('')
+    }
 
-        setComments([...comments, comments.length + 1]);
-
+    function handleNewCommentChange() {
+       setNewCommentText(event.target.value);
     }
 
     return (
@@ -57,7 +62,10 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
+                    name='comment'
                     placeholder="Deixe um comentário"
+                    value={newCommentText} 
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -67,7 +75,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment} />
                 })}
                 
             </div>
