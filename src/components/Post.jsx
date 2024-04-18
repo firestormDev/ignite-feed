5f676fb02fd88720874de463a5ed4554b4b64905
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2024-04-17 22:16:42 -0300

    fix: key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,7 +53,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph'){
-                        return <p><a href="">{line.content}</a></p>
+                        return <p key={line.content}><a href="">{line.content}</a></p>
+                    } else if (line.type === 'paragraph'){
+                        return <p key={line.content}><a href="">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -75,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
                 
             </div>
